12269dc839341f637041c9297faef44d7b214663
Marcelo Negrão <marcelomascarenhas73@gmail.com>
2026-06-07 23:25:11 -0300

    feat: inicia integracao com PostgreSQL

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
-from app.routes import router
 from strawberry.fastapi import GraphQLRouter
+
+from app.routes import router
 from app.graphql_schema import schema
+
+from app.database import engine
+from app.models import Base
+
+# Cria as tabelas do banco automaticamente
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FastAPI GraphQL API")
 
+# Rotas REST
 app.include_router(router)
 
+# Rotas GraphQL
 graphql_app = GraphQLRouter(schema)
 app.include_router(graphql_app, prefix="/graphql")
---
+++ app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+
+class PostModel(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
